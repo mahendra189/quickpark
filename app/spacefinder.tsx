1fc5a978e65940e13f3a7073ac3af5965173e421
mahendra189 <mahendrank18082004@gmail.com>
2025-06-24 23:50:32 +0530

    Done with Home and Space Finder

--- app/spacefinder.tsx
+++ app/spacefinder.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { Platform, Pressable, SafeAreaView, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { Platform, Pressable, SafeAreaView, ScrollView, StyleSheet, View, Image } from 'react-native';
 import { Box } from '@/components/ui/box';
 import { Text } from '@/components/ui/text';
 import {
@@ -15,36 +15,216 @@ import {
 import { Button, ButtonText } from '@/components/ui/button';
 import { StatusBar } from 'expo-status-bar';
 import { Icon } from '@/components/ui/icon';
-import { ArrowLeft, Backpack } from 'lucide-react-native';
+import { ArrowLeft, ArrowRight, Backpack, LocateFixed, LocateFixedIcon, LocationEdit, Search } from 'lucide-react-native';
 import { useRouter } from 'expo-router';
+import { Input, InputField } from '@/components/ui/input';
+import Location from 'expo-location';
+import { HStack } from '@/components/ui/hstack';
+import { Heading } from '@/components/ui/heading';
 const Space = () => {
   const router = useRouter();
   const [showActionsheet, setShowActionsheet] = React.useState(false)
   const handleClose = () => setShowActionsheet(false)
+  const [location, setLocation] = React.useState({ latitude: 18.5204, longitude: 73.8567 });
 
+  const [availableSpaces, setAvailableSpaces] = useState([
+
+
+    {
+      id: 1,
+      name: 'Grünerløkka',
+      address: '14 Thorvald Meyers Gate',
+      distance: '1.2 km away',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/hardangerfjord-in-south-western-norway-in-the-royalty-free-image-1620377261.'
+    },
+    {
+      id: 2,
+      name: 'Bryggen',
+      address: '22 Bryggen Street',
+      distance: '3.4 km away',
+      image: 'https://media.istockphoto.com/id/1500420309/photo/perfect-reflection-of-the-reine-village-on-the-water-of-the-fjord-in-the-lofoten-islands.jpg?s=612x612&w=0&k=20&c=4FMvHEkG3FH6fpaE355CnUSdXtV4LlqCBF9etWbNR6o='
+    },
+    {
+      id: 3,
+      name: 'Tromsøya',
+      address: '9 Langnesvegen, Tromsø',
+      distance: '4.8 km away',
+      image: 'https://media.istockphoto.com/id/1500420309/photo/perfect-reflection-of-the-reine-village-on-the-water-of-the-fjord-in-the-lofoten-islands.jpg?s=612x612&w=0&k=20&c=4FMvHEkG3FH6fpaE355CnUSdXtV4LlqCBF9etWbNR6o='
+    },
+    {
+      id: 4,
+      name: 'Trondheim Sentrum',
+      address: '67 Olav Tryggvasons Gate, Trondheim',
+      distance: '2.9 km away',
+      image: 'https://images.unsplash.com/photo-1576420801685-e9d3fd6c6b5c'
+    },
+    {
+      id: 5,
+      name: 'Aker Brygge',
+      address: '3 Stranden, Oslo',
+      distance: '2.1 km away',
+      image: 'https://images.unsplash.com/photo-1561489424-b51c2b3b6ad4'
+    },
+    {
+      id: 6,
+      name: 'Geiranger',
+      address: '11 Fjordgata, Geiranger',
+      distance: '8.5 km away',
+      image: 'https://images.unsplash.com/photo-1572635196237-6efb894d6075'
+    },
+    {
+      id: 7,
+      name: 'Svolvær',
+      address: '39 Havnveien, Lofoten',
+      distance: '6.4 km away',
+      image: 'https://images.unsplash.com/photo-1523544261120-22c97f86e8f3'
+    },
+    {
+      id: 8,
+      name: 'Tjuvholmen',
+      address: '5 Tjuvholmen allé, Oslo',
+      distance: '0.7 km away',
+      image: 'https://images.unsplash.com/photo-1637067315179-c3fbe03a70e3'
+    },
+    {
+      id: 9,
+      name: 'Stavanger',
+      address: '10 Kirkegata, Stavanger',
+      distance: '3.0 km away',
+      image: 'https://images.unsplash.com/photo-1586297135537-94bc9ba8f2ef'
+    },
+    {
+      id: 10,
+      name: 'Ålesund',
+      address: '20 Apotekergata, Ålesund',
+      distance: '2.2 km away',
+      image: 'https://images.unsplash.com/photo-1611600749571-cdfbb502ec43'
+    }
+
+
+  ]);
+
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    });
+    console.log('Current location:', location);
+  }
+
+  useEffect(() => {
+    (async () => {
+      getCurrentLocation();
+    })();
+  }, []);
   return (
     <SafeAreaView className="flex-1 bg-background-100">
       <StatusBar style={Platform.OS === "ios" ? "dark" : "auto"} backgroundColor="#1E1B22" />
 
       <MapView style={{ flex: 1 }} initialRegion={{
-        latitude: 18.5204,
-        longitude: 73.8567,
+        latitude: location.latitude,
+        longitude: location.longitude,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}>
+        <Marker style={{ width: 30, height: 30 }} coordinate={{ latitude: location.latitude, longitude: location.longitude }} title="Pune" description="Sample Marker for App." />
 
       </MapView>
-      <Box className='absolute top-14 left-0 flex-row right-0 rounded-md p-4'>
-        <Pressable onPress={() => {
-          router.back();
-        }} className='bg-background-0 border border-typography-500 rounded-full p-2'>
+      <Box className='absolute top-12 flex-row items-center justify-between p-4 gap-2'>
 
-          <Icon as={ArrowLeft} color="black" size='xl' />
+        <Box className=' flex-row rounded-full'>
+          <Pressable
+            android_ripple={{ color: 'black', borderless: true }}
+            onPress={() => {
+              router.back();
+            }} className='bg-background-0 border border-typography-500 rounded-full p-4'>
+
+            <Icon as={ArrowLeft} color="black" size='xl' />
+          </Pressable>
+        </Box>
+        <Box className='flex-row items-center w-[85%] bg-background-0 border border-typography-500 rounded-full'>
+          <Pressable
+            android_ripple={{ color: 'black', borderless: true }}
+            onPress={() => {
+              console.log("Backpack Pressed");
+
+            }} className='flex-row bg-background-0 border-0 border-typography-500 rounded-full p-4'>
+
+            <Icon as={Search} color="black" size='xl' />
+          </Pressable>
+          <Input className="flex-1 border-0 focus:border-0 focus:ring-0 text-white" >
+            <InputField placeholder="Search..." />
+          </Input>
+        </Box>
+      </Box>
+      <Box className='absolute bottom-10 right-0 flex-row items-center justify-between p-4 gap-y-2'>
+        <Pressable
+          android_ripple={{ color: 'black', borderless: true }}
+          onPress={() => {
+            console.log("Locate Pressed");
+          }} className='bg-background-0 border border-typography-500 rounded-full p-4'>
+
+          <Icon as={LocateFixedIcon} color="black" size='xl' />
         </Pressable>
       </Box>
-      <Button className='absolute bottom-10 left-0 right-0 mx-4' onPress={() => setShowActionsheet(true)}>
+      {
+        availableSpaces.length === 0 ? (
+          <Box className='absolute bottom-36 left-0 right-0 gap-y-2 p-2 h-64 mx-3 flex-1  rounded-2xl overflow-hidden border border-typography-400 bg-background-0 flex-row items-center justify-center'>
+            <Box className='items-center justify-center flex-1'>
+              <Image
+                source={require('../assets/images/noparking.png')}
+                style={{ width: 120, height: 120 }}
+                resizeMode="contain"
+              />
+              <Text className='text-typography-500 text-center mt-4'>
+                No parking spaces found nearby.
+              </Text>
+            </Box>
+          </Box>
+        ) : (
+          <ScrollView className='absolute bottom-0 left-0 right-0 gap-y-2 p-2' contentContainerStyle={{ paddingBottom: 100 }} showsHorizontalScrollIndicator={false} horizontal={true}>
+            <HStack space='sm'>
+              {
+
+                availableSpaces.map((space) => (
+                  <Box key={space.id} className='w-64 h-64 rounded-2xl overflow-hidden border border-typography-400 bg-background-0'>
+                    <Box className='h-[40%] bg-gradient-to-b from-background-0 to-background-100 flex-row items-center justify-between'>
+                      <Image source={{ uri: space.image }} style={{ width: "100%", height: '100%' }} alt={space.name} />
+                    </Box>
+                    <Box className='p-4 overflow-hidden'>
+                      <Heading numberOfLines={1} ellipsizeMode="tail">
+                        {space.name}
+                      </Heading>
+                      <Text className='text-typography-500' numberOfLines={2} ellipsizeMode="tail">
+                        {space.address}
+                      </Text>
+                      <Text className='text-typography-500' numberOfLines={1}>
+                        {space.distance}
+                      </Text>
+                      <Pressable
+                        className='my-1 w-10 h-10 flex-row items-center justify-center border border-typography-0 rounded-full p-4 bg-orange'
+                        onPress={() => setShowActionsheet(true)}
+                      >
+                        <Icon as={ArrowRight} color="white" size='md' />
+                      </Pressable>
+                    </Box>
+                  </Box>
+                ))
+              }
+            </HStack>
+          </ScrollView>
+        )
+      }
+
+      {/* <Button className='absolute bottom-10 left-0 right-0 mx-4' onPress={() => setShowActionsheet(true)}>
         <ButtonText>Find Spaces</ButtonText>
-      </Button>
+      </Button> */}
       <Actionsheet isOpen={showActionsheet} onClose={handleClose}>
         <ActionsheetBackdrop />
         <ActionsheetContent>
